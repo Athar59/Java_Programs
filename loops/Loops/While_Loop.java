cf897355e315968f073a39a412680aaef9adfb7c
Mohammad Atharuddin <mdathar7262@gmail.com>
2025-02-28 01:40:25 +0530

    Added loops with code files

---
+++ loops/Loops/While_Loop.java
@@ -0,0 +1,20 @@
+
+
+public class While_Loop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+//OUTPUT******************
+
+// 1
+// 2
+// 3
+// 4
+// 5
+
