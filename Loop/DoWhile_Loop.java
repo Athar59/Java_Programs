2af4f43abf979dc1a8d8c40e118741659276ddfd
Mohammad Atharuddin <mdathar7262@gmail.com>
2025-02-28 01:44:20 +0530

    Renamed Loops/Loops to Loop

---
+++ Loop/DoWhile_Loop.java
@@ -0,0 +1,16 @@
+
+public class DoWhile_Loop {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
+    }
+}
+
+
+// OUTPUT ***************
+
+// 4
+// 5
